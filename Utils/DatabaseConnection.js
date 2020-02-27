812f422f188d9e7b49a0c08821f2c9f17d4cfdff
JigneshRajput <46437074+JigneshRajput@users.noreply.github.com>
2020-02-27 17:30:58 +0530

    Update DatabaseConnection.js

--- Utils/DatabaseConnection.js
+++ Utils/DatabaseConnection.js
@@ -2,10 +2,10 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize(process.env.Mysqldatabase, process.env.Mysqluser, process.env.Mysqlpassword, {
-    host: process.env.MysqlHost,
+var sequelize = new Sequelize(MyDB, User, Password, {
+    host: HOST,
     dialect: 'mysql',
-    port: process.env.MysqlPort,
+    port: PORT,
     pool: {
         max: 5,
         min: 0,
